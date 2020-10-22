d0fa057e7da789800d1719e064419997751bb76f
Sasha Ivanova <aliaksandra.v.ivanova@gmail.com>
2020-10-22 22:20:39 +0200

    draft is ready

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,34 @@
 import React from 'react';
-import logo from '../../logo.svg';
+
 import './App.css';
+
+class App extends React.Component{
+
+  constructor(){
+
+  }
+
+  render(){
+    return(
+
+      <Header />
+
+      <Search />
+
+      <Main />
+
+      <About />
+
+      <Footer />
+    )
+  }
+
+
+
+
+}
+
+
 
 function App() {
   return (
