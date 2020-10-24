704e2a5ab68cd6b16fe5b9b010c06bdacbe4bee6
Sasha Ivanova <aliaksandra.v.ivanova@gmail.com>
2020-10-24 12:46:33 +0200

    search part

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -7,7 +7,10 @@ function Search () {
         <div className="search">
             <h1 className="search__title">Что творится в мире?</h1>
             <h2 className="search__subtitle">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</h2>
-            <p className="search__field">Введите тему новости</p>
+                <div className="search__wrapper">
+                    Введите тему новости
+                    <p className="search__button">Искать</p>
+                </div>
         </div>
     );
 }
