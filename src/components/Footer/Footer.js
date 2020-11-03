e65099e10a9188d6529d84d7e5fa56495f935766
Sasha Ivanova <aliaksandra.v.ivanova@gmail.com>
2020-11-03 23:30:42 +0100

    saved-news adaptive is done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,11 +10,14 @@ function Footer () {
         <footer className="footer">
             <p className="footer__name">&copy; 2020 Supersite, Powered by News API</p>
             <div className="footer__menu">
-                <Link to="/" exact className="footer__menu-item" >
-                    Главная
-                 </Link>
-                <a className="footer__menu-item" href="https://praktikum.yandex.ru/">Яндекс.Практикум</a>
-                <a className="footer__menu-icon" href="https://github.com/AliaksandraI"><img src={githubIcon} alt="GitHub иконка"/></a>
+                <div className="footer__menu-wrapper">
+                    <Link to="/" exact className="footer__menu-item" >
+                        Главная
+                    </Link>
+                    <a className="footer__menu-item" href="https://praktikum.yandex.ru/">Яндекс.Практикум</a>
+                </div>
+                    <a className="footer__menu-icon footer__menu-icon-git" href="https://github.com/AliaksandraI"><img src={githubIcon} alt="GitHub иконка"/></a>
+
                 <a className="footer__menu-icon" href="https://www.facebook.com/profile.php?id=100009930902023"><img src={fbIcon} alt="GitHub иконка"/></a>
             </div>
             
